b344538cd22ff4b7c4876773ae577ed3fed6d2ac
adriennebergh <adriennebergh10@gmail.com>
2016-01-08 17:45:49 -0800

    Create Cell.h

---
+++ Cell.h
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Cell
+{
+	public:
+		Cell();
+		virtual ~Cell();
+		
+		double popDensity;
+		int numCells;
+
+		void Quit();
+		void printFlipConsole(int **arrB, int w, int h, int looping);
+		void printConsole(int **arrB, int w, int h, int looping);
+
+		void numNeighborsClassic(int **arr, int **arrB, int w, int h, bool fType, bool out, int Loop);
+		void numNeighborsMirror(int **arr, int **arrB, int w, int h, bool fType, bool out, int Loop);
+		void numNeighborsDonut(int **arr, int **arrB, int w, int h, bool fType, bool out, int Loop);
+		
+		void initialPop(int **arr, int w, int h, double d);
+		void initialPop_file(int **arr, const char* fileName);
+
+};
